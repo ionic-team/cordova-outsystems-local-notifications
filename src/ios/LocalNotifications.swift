8ec402611230101f4a6409b0e0cf8c9fa80be85b
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2026-08-12 18:43:33 -0300

    chore: removed unused error

--- src/ios/LocalNotifications.swift
+++ src/ios/LocalNotifications.swift
@@ -8,7 +8,6 @@ enum LocalNotificationsError: Error {
     case missingIdentifier
     case contentBuildFailed
     case triggerBuildFailed
-    case scheduleInPast
     case notificationsDisabled
     case invalidColor
     case invalidRemoveList
@@ -22,13 +21,12 @@ enum LocalNotificationsError: Error {
         case .missingIdentifier: return "OS-PLUG-LNOT-0002"
         case .contentBuildFailed: return "OS-PLUG-LNOT-0003"
         case .triggerBuildFailed: return "OS-PLUG-LNOT-0004"
-        case .scheduleInPast: return "OS-PLUG-LNOT-0005"
-        case .notificationsDisabled: return "OS-PLUG-LNOT-0006"
-        case .invalidColor: return "OS-PLUG-LNOT-0007"
-        case .invalidRemoveList: return "OS-PLUG-LNOT-0012"
-        case .missingIds: return "OS-PLUG-LNOT-0013"
-        case .scheduleFailed: return "OS-PLUG-LNOT-0011"
-        case .permissionRequestFailed: return "OS-PLUG-LNOT-0014"
+        case .notificationsDisabled: return "OS-PLUG-LNOT-0005"
+        case .invalidColor: return "OS-PLUG-LNOT-0006"
+        case .invalidRemoveList: return "OS-PLUG-LNOT-0011"
+        case .missingIds: return "OS-PLUG-LNOT-0012"
+        case .scheduleFailed: return "OS-PLUG-LNOT-0010"
+        case .permissionRequestFailed: return "OS-PLUG-LNOT-0013"
         }
     }
 
@@ -38,7 +36,6 @@ enum LocalNotificationsError: Error {
         case .missingIdentifier: return "Notification is missing an identifier."
         case .contentBuildFailed: return "Unable to build the notification content."
         case .triggerBuildFailed: return "Unable to create the notification, trigger construction failed."
-        case .scheduleInPast: return "Scheduled time must be after the current time."
         case .notificationsDisabled: return "Notifications are not enabled on this device."
         case .invalidColor: return "Invalid color provided. Must be a hex string (e.g. #ff0000)."
         case .invalidRemoveList: return "Expected notifications to be a list of notification objects."
